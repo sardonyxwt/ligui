3e694cd49e73d374088a81359474f6a4f7501f25
Kirill Petkov <sardonyx.wt@gmail.com>
2018-02-11 13:00:34 +0200

    update statistic report

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,59 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const VisualizerWebpackPlugin = require('webpack-visualizer-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const dist = "./dist";
 
-const plugins = [
-  new CleanWebpackPlugin(dist),
-  new BundleAnalyzerPlugin({
-    openAnalyzer: false,
-    analyzerMode: 'static',
-    reportFilename: `${__dirname}/bundle-report.html`
-  })
-];
+module.exports = env => {
+  const plugins = [
+    new CleanWebpackPlugin(dist)
+  ];
 
-const rules = [
-  {
-    test: /\.tsx?$/,
-    exclude: /node_modules/,
-    loader: 'ts-loader'
-  },
-  {
-    test: /\.tsx?$/,
-    enforce: 'pre',
-    loader: 'tslint-loader',
-    options: {
-      emitErrors: true,
-      fix: true
-    }
+  if(env['production']) {
+    plugins.push(
+      new VisualizerWebpackPlugin({
+        filename: './bundle-statistics.html'
+      }),
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+        analyzerMode: 'static',
+        reportFilename: './bundle-report.html'
+      })
+    )
   }
-];
 
-module.exports = {
-  entry: './src/index.ts',
-  devtool: 'source-map',
-  output: {
-    filename: `${dist}/ligui.min.js`,
-    library: "ligui",
-    libraryTarget: 'umd',
-    umdNamedDefine: true
-  },
-  resolve: {
-    extensions: ['.js', '.ts']
-  },
-  module: {
-    rules
-  },
-  plugins
+  const rules = [
+    {
+      test: /\.tsx?$/,
+      exclude: /node_modules/,
+      loader: 'ts-loader'
+    },
+    {
+      test: /\.tsx?$/,
+      enforce: 'pre',
+      loader: 'tslint-loader',
+      options: {
+        emitErrors: true,
+        fix: true
+      }
+    }
+  ];
+
+  return {
+    entry: './src/index.ts',
+    devtool: 'source-map',
+    output: {
+      filename: `${dist}/ligui.min.js`,
+      library: "ligui",
+      libraryTarget: 'umd',
+      umdNamedDefine: true
+    },
+    resolve: {
+      extensions: ['.js', '.ts']
+    },
+    module: {
+      rules
+    },
+    plugins
+  }
 };
