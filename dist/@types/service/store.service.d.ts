08d398aa933a32e98b1e97c7e33ad5e45880545d
Kirill Petkov <sardonyx.wt@gmail.com>
2018-08-12 11:38:03 +0300

    increment version 2.3.63 => 2.4.1 - update @sardonyxwt/state-store version to 2.8.11 - update store.service.ts

--- dist/@types/service/store.service.d.ts
+++ dist/@types/service/store.service.d.ts
@@ -1,9 +1,10 @@
-import { Scope } from '@sardonyxwt/state-store';
+import { Scope, ScopeMiddleware, StoreDevTool } from '@sardonyxwt/state-store';
 export interface StoreService {
-    createScope<T>(name?: string, initState?: T): Scope<T>;
-    composeScope(name: string, scopes: (Scope | string)[]): Scope;
+    createScope<T>(name?: string, initState?: T, middleware?: ScopeMiddleware<T>[]): Scope<T>;
+    composeScope(name: string, scopes: (Scope | string)[], middleware?: ScopeMiddleware[]): Scope;
     getScope(scopeName: string): Scope;
     getState(): {};
+    setStoreDevTool(devTool: StoreDevTool): void;
     ROOT_SCOPE: Scope<{}>;
 }
 export declare const storeService: StoreService;
--- src/service/store.service.ts
+++ src/service/store.service.ts
@@ -1,10 +1,14 @@
-import { createScope, composeScope, getScope, getState, ROOT_SCOPE, Scope } from '@sardonyxwt/state-store';
+import {
+  createScope, composeScope, getScope, getState, setStoreDevTool, ROOT_SCOPE,
+  Scope, ScopeMiddleware, StoreDevTool
+} from '@sardonyxwt/state-store';
 
 export interface StoreService {
-  createScope<T>(name?: string, initState?: T): Scope<T>;
-  composeScope(name: string, scopes: (Scope | string)[]): Scope;
+  createScope<T>(name?: string, initState?: T, middleware?: ScopeMiddleware<T>[]): Scope<T>;
+  composeScope(name: string, scopes: (Scope | string)[], middleware?: ScopeMiddleware[]): Scope;
   getScope(scopeName: string): Scope;
   getState(): {};
+  setStoreDevTool(devTool: StoreDevTool): void;
   ROOT_SCOPE: Scope<{}>;
 }
 
@@ -12,12 +16,12 @@ class StoreServiceImpl implements StoreService {
 
   ROOT_SCOPE = ROOT_SCOPE;
 
-  createScope<T>(name?: string, initState?: T) {
-    return createScope(name, initState);
+  createScope<T>(name?: string, initState?: T, middleware?: ScopeMiddleware<T>[]) {
+    return createScope(name, initState, middleware);
   }
 
-  composeScope(name: string, scopes: (Scope | string)[]) {
-    return composeScope(name, scopes);
+  composeScope(name: string, scopes: (Scope | string)[], middleware?: ScopeMiddleware[]) {
+    return composeScope(name, scopes, middleware);
   }
 
   getScope(scopeName: any) {
@@ -26,6 +30,10 @@ class StoreServiceImpl implements StoreService {
 
   getState() {
     return getState();
+  }
+
+  setStoreDevTool(devTool: StoreDevTool) {
+    setStoreDevTool(devTool)
   }
 
 }
