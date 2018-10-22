6cb7173356f7db236a10dae6eccb33ceac981fca
Kirill Petkov <sardonyx.wt@gmail.com>
2018-10-22 12:40:56 +0300

    increment version 3.2.4 => 3.2.5 - fix fetch service

---
+++ dist/@types/hoc/subscribe.hoc.d.ts
@@ -0,0 +1,6 @@
+import * as React from 'react';
+export interface SubscribeScopeSetting {
+    scopeName: string;
+    actions?: string[];
+}
+export declare function subscribe<T>(injectedScopeStates: (string | SubscribeScopeSetting)[]): <TOriginalProps extends {}>(Component: React.ComponentType<TOriginalProps>) => any;
