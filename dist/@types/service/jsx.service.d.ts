41317af6c069d4c53c14425f08de5973239c3bb3
Kirill Petkov <sardonyx.wt@gmail.com>
2018-11-27 14:32:16 +0200

    increment version 4.2.53 => 4.2.54 - optimize jsx service

--- dist/@types/service/jsx.service.d.ts
+++ dist/@types/service/jsx.service.d.ts
@@ -1,10 +1,9 @@
 import * as React from 'react';
 export interface JSXService {
-    registerFactory<T extends {}>(name: string, factory: React.Factory<T>): JSXService;
+    registerFactory<T extends {}>(name: string, factory: React.Factory<T>): void;
     node<T extends {}>(name: string, props?: T, ...children: React.ReactNode[]): React.ReactElement<T>;
     render<T extends {}>(container: Element, element: React.ReactElement<T>): any;
     renderComponent<T extends {}>(container: Element, name: string, props?: T, ...children: React.ReactNode[]): void;
-    ['render*']?(container: Element, props?: any, children?: React.ReactNode[]): void;
     classes(classes: (string | [string, boolean])[]): string;
 }
 export declare const jsxService: JSXService;
--- src/service/jsx.service.ts
+++ src/service/jsx.service.ts
@@ -2,11 +2,10 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 export interface JSXService {
-  registerFactory<T extends {}>(name: string, factory: React.Factory<T>): JSXService;
+  registerFactory<T extends {}>(name: string, factory: React.Factory<T>): void;
   node<T extends {}>(name: string, props?: T, ...children: React.ReactNode[]): React.ReactElement<T>;
   render<T extends {}>(container: Element, element: React.ReactElement<T>);
   renderComponent<T extends {}>(container: Element, name: string, props?: T, ...children: React.ReactNode[]): void;
-  ['render*']?(container: Element, props?, children?: React.ReactNode[]): void;
   classes(classes: (string | [string, boolean])[]): string;
 }
 
@@ -16,17 +15,7 @@ const registerFactory = <T extends {}>(name: string, factory: React.Factory<T>) 
   if (name in factories) {
     throw new Error(`Factory with same name is register.`);
   }
-  if (name in this) {
-    throw new Error(`Factory not registered, because name: ${name} is reserved.`);
-  }
-  const capitalizeFirstLetterActionName = () => {
-    return name.charAt(0).toUpperCase() + name.slice(1);
-  };
   factories[name] = factory;
-  this[`render${capitalizeFirstLetterActionName()}`] = (container: Element, props?, children?: React.ReactNode[]) => {
-    render(container, factory(props, children))
-  };
-  return this;
 };
 
 const node = <T extends {}>(name: string, props?: T, ...children: React.ReactNode[]) => {
@@ -60,7 +49,7 @@ const classes = (classes: (string | [string, boolean])[]) => {
 };
 
 export const jsxService: JSXService = Object.freeze({
-  registerFactory: registerFactory.bind(this),
+  registerFactory,
   node,
   render,
   renderComponent,
