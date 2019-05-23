cda754b3a1530e5b969bc024502eafbb02afb9f5
Kirill Petkov <sardonyx.wt@gmail.com>
2019-05-23 15:43:04 +0300

    increment version 8.0.6 => 8.0.7 - refactor

--- src/service/module.service.ts
+++ src/service/module.service.ts
@@ -1,7 +1,6 @@
 import { inject, injectable } from 'inversify';
 import autobind from 'autobind-decorator';
 import { LIGUI_TYPES } from '../types';
-import { SynchronousPromise } from 'synchronous-promise';
 
 export type ModuleLoader = (key: string, cb: (module: any) => void) => void;
 
@@ -43,7 +42,7 @@ export class ModuleServiceImpl implements ModuleService {
 
     if (!(key in _modulePromises)) {
       if (_modules[key]) {
-        _modulePromises[key] = SynchronousPromise.resolve(_modules[key]);
+        _modulePromises[key] = Promise.resolve(_modules[key]);
       } else {
         _modulePromises[key] = new Promise(resolve => _loader(key, resolve))
           .then(module => _modules[key] = module);
