a7e5ebc2db032f600da859f3bc84fb9fef10a56b
Kyrylo Petkov <sardonyx.wt@gmail.com>
2020-02-26 18:28:48 +0200

    - remove navigation components

--- dist/@types/navigation/link.component.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import { LinkProps } from 'react-router-dom';
-export declare function Link({ to, replace, innerRef, onClick, ...anchorProps }: LinkProps): JSX.Element;
-//# sourceMappingURL=link.component.d.ts.map
--- dist/@types/navigation/redirect.component.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-import { RedirectProps } from 'react-router';
-export { RedirectProps };
-export declare function Redirect(props: RedirectProps): JSX.Element;
-//# sourceMappingURL=redirect.component.d.ts.map
